5c5e2179d205472b823fb1755c6b33ca32cb3477
Maksym Karalash <Maks.karalash@gmail.com>
2023-03-30 18:36:43 +0300

    task-2 DONE

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const imageMarkup = galleryItems.map(({preview, original, description}) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}" title="${description}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup)
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: 'self',
+    captionDelay: 250,
+});
+
+galleryEl.addEventListener('click', onOpenModalImg)
+
+console.log(lightbox);
+function onOpenModalImg(e) {
+    if (e.target.classList.value !== 'gallery__image') {
+        return;
+    }
+    e.preventDefault()
+}
